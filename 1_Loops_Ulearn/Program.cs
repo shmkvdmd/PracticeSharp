003d4a69953972bd990eda09d5ccb480430b02e0
shmkvdmd <demidka100@mail.ru>
2023-08-17 16:27:28 +0300

    Добавьте файлы проекта.

---
+++ 1_Loops_Ulearn/Program.cs
@@ -0,0 +1,25 @@
+﻿//Дано N (1 ≤ N ≤ 27). Найти количество трехзначных натуральных чисел, сумма цифр которых равна N.
+//Операции деления (/, %) не использовать.
+double SumDigits(string iteration)
+{
+    double count = 0;
+    for (int i = 0; i < iteration.Length; i++)
+    {
+        count += Char.GetNumericValue(iteration, i);
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число от 1 до 27");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 100; i <= 999; i++)
+{
+    string check = i.ToString();
+    if (number == SumDigits(check))
+    {
+        count++;
+        Console.WriteLine(check);
+    }
+}
+Console.WriteLine(count);
